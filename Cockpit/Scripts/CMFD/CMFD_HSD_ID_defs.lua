23cafc12d03043782caaba45531088509e992d27
5298125985218 <81524965+5298125985218@users.noreply.github.com>
2022-03-05 18:40:03 -0300

    Added declutter features and subformats to HSD.

---
+++ Cockpit/Scripts/CMFD/CMFD_HSD_ID_defs.lua
@@ -0,0 +1,18 @@
+CMFD_HSD_FORMAT_IDS = {
+    HSD = 0,
+    DL = 1,
+    DCTL = 2,
+    ROUTE = 3
+}
+
+CMFD_HSD_DL_IDS = {
+    ALL = 0,
+    FORM = 1,
+    TGT = 2,
+    NONE = 3
+}
+
+CMFD_HSD_DL_IDS.ALL = "DL\nALL"
+CMFD_HSD_DL_IDS.FORM = "DL\nFORM"
+CMFD_HSD_DL_IDS.TGT = "DL\nTGT"
+CMFD_HSD_DL_IDS.NONE = "DL\nNONE"
